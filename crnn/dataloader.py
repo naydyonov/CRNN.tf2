b0d5f74ce527ed81e57faf1a5252d2ef93563ee9
a.naydyonov@gmail.com <a.naydyonov@gmail.com>
2020-10-23 16:12:51 +0000

    еуые

---
+++ crnn/dataloader.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+class DataLoader:
+    def __init__(self, img_channels, max_img_width=300, 
+                 ignore_case=False, img_width=128, img_height=1024):
+        self.img_channels = img_channels
+        self.img_width = img_width
+        self.img_height = img_height
+
+    def build_labels:
+        pass
